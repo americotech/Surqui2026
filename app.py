37927f34aa18f67ef894a26db7b8e29e6e4a04a1
americotech <americotech@gmail.com>
2026-04-18 01:17:51 -0500

    Fix: Remove psycopg2 imports from top level to avoid local errors

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor  # <--- Asegúrate de que esta línea esté clara
+import sqlite3
 import datetime
 import calendar
 from collections import defaultdict
